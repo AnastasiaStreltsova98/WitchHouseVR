1fc824a734c3f031f5f3a22e0b9a5e774b3da42c
Streltsova Anastasia <anastasia.streltsova98@gmail.com>
2021-06-21 22:00:14 +1000

    add changing color

---
+++ Assets/Scripts/changeFluidColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeFluidColor : MonoBehaviour
+{
+    public GameObject Fluid;
+    public GameObject[] Bubbles;
+    //public Color redcolor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fluid = GameObject.FindGameObjectWithTag("fluid");
+        Bubbles = GameObject.FindGameObjectsWithTag("bubble");
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("bottle"))
+        {
+            Debug.Log(other.gameObject.name);
+            Color gColor = other.gameObject.GetComponent<Renderer>().materials[0].GetColor("_Color");
+            Fluid.GetComponent<Renderer>().materials[0].SetColor("_FarColor", gColor);
+            Fluid.GetComponent<Renderer>().materials[0].SetColor("_SunSpecularColor", gColor);
+            Fluid.GetComponent<Light>().color = gColor;
+            foreach (GameObject bubble in Bubbles)
+            {
+                bubble.GetComponent<Renderer>().materials[0].color = gColor;
+                bubble.GetComponent<Renderer>().materials[0].SetColor("_EmissionColor", gColor);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
